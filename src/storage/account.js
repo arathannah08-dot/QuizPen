80f54cbbc20a1a3dee8c6494850fd543b8c43dea
arathannah08-dot <arathannah08@gmail.com>
2026-09-24 23:26:30 +0800

    cleaned up account.js

--- src/storage/account.js
+++ src/storage/account.js
@@ -1,9 +1,6 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 2f859a3 (Update features)
 export const saveAccount = async (account) => {
     try {
         await AsyncStorage.setItem('account', JSON.stringify(account));
@@ -12,33 +9,20 @@ export const saveAccount = async (account) => {
     }
 };
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 2f859a3 (Update features)
 export const getAccount = async () => {
     try {
         const data = await AsyncStorage.getItem('account');
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 2f859a3 (Update features)
         if (data) {
             return JSON.parse(data);
         }
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 2f859a3 (Update features)
         return null;
     } catch (error) {
         console.log('Error getting account:', error);
         return null;
     }
-<<<<<<< HEAD
 };
-=======
-};
->>>>>>> 2f859a3 (Update features)
