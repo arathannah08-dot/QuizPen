70508a1451f15c87ecd8bfd00d14430e4a5e3693
arathannah08-dot <arathannah08@gmail.com>
2026-09-25 00:06:53 +0800

    Updated createquiz.jsx

--- src/app/(tabs)/createquiz.jsx
+++ src/app/(tabs)/createquiz.jsx
@@ -1,39 +1,100 @@
-import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { ScrollView, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import { useState} from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import ActButton from '../../components/ui/ActButton';
 import UserInput from '../../components/ui/UserInput';
 import AddSign from '../../components/ui/AddSign';
 import Add from '../../components/ui/Add';  
-import { useRouter } from 'expo-router'; 
+import { useRouter } from 'expo-router';
+import { saveQuiz } from '../../storage/quiz';
 
 export default function CreateQuiz() {
   const router = useRouter();
+
+  const [title, setTitle] = useState('');
+  const [desc, setDesc] = useState('');
+  const [quiz, setQuiz] = useState([
+    {
+      question: '',
+      answer: ''
+    }
+  ]);
+
+  const addQuiz = () => {
+    setQuiz([...quiz,
+      {
+        question: '',
+        answer: '',
+      }
+    ]);
+  };
+
   return (
     <SafeAreaView>
+      <ScrollView>
         <Text>Create Quiz</Text>
 
         <Text>Add Title</Text>
-        <UserInput type="Title"/>
+        <UserInput 
+         type="Title"
+         value={title}
+         onChangeText={setTitle}/>
+
         <Text>Add Description</Text>
-        <UserInput type="Description"/>
+        <UserInput 
+         type="Description"
+         value={desc}
+         onChangeText={setDesc}/>
 
         <AddSign 
          icon="add-circle"
          size={30}
          color="darkblue"
-         onPress={() => console.log("Add pressed")}
+         onPress={addQuiz}
         />
-        <Text>Add Question</Text>
-        <Add add="Question"/>
+        
+        {quiz.map((item, index) => (
+          <View key={index}>
+            
+            <Text>Add Question</Text>
+            <Add 
+             add="Question"
+             value={item.question}
+             onChangeText={(text) => {
+              const updatedQuiz = [...quiz];
+              updatedQuiz[index] = text;
+              setQuiz(updatedQuiz);
+             }}
+            />
 
-        <Text>Add Answer</Text>
-        <Add add="Answer"/>
+            <Text>Add Answer</Text>
+            <Add 
+             add="Answer"
+             value={item.answer}
+             onChangeText={(text) => {
+              const updatedQuiz = [...quiz];
+              updatedQuiz[index] = text;
+              setQuiz(updatedQuiz);
+             }}
+            />
+
+          </View>
+        ))}
 
         <ActButton 
-         name="Save Quiz"
-         onPress={() => router.push('/(tabs)/quizzes')}
+         name="Save Quiz Set"
+         onPress={async() => {
+          await saveQuiz({
+            title: title,
+            desc: desc,
+            quiz: quiz
+          });
+
+          router.push('/(tabs)/quizzes');
+         }}
         />
 
+      </ScrollView>
     </SafeAreaView>
   );
 }
