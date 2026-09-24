047b69930a6aea9adf9a0ecd55740ffc4690e7f8
arathannah08-dot <arathannah08@gmail.com>
2026-09-24 23:25:33 +0800

    Used map for displaying flashcards array

--- src/app/(tabs)/flashcards.jsx
+++ src/app/(tabs)/flashcards.jsx
@@ -1,15 +1,36 @@
-import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import {useEffect, useState} from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import UserInput from '../../components/ui/UserInput'; 
 import { useRouter } from 'expo-router'; 
+import {getFlashcard} from "../../storage/flashcard";
 
 export default function Flashcards() {
   const router = useRouter();
+
+  const [flashcards, setFlashcards] = useState([]);
+
+  useEffect (() => {
+    const loadFlashcards = async () => {
+      const data = await getFlashcard();
+      setFlashcards(data);
+    };
+
+    loadFlashcards();
+  }, []);
+
   return (
     <SafeAreaView>
       <Text>Flashcards</Text>
       
       <UserInput type="Search flashcard ..."/>
+
+      {flashcards.map((item, index) => (
+        <TouchableOpacity key={index}>
+          <Text>{item.title}</Text>
+          <Text>{item.desc}</Text>
+        </TouchableOpacity>
+      ))}
 
     </SafeAreaView>
   );
